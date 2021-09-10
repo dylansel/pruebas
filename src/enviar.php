839d4b320acf0a0754f66b8d9042fbacc864b69b
dylansel <dylansel@users.noreply.github.com>
2021-09-10 11:33:02 -0300

    AAA

---
+++ src/enviar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email</title>
+    <link rel="stylesheet" href="src/app.css">
+</head>
+<body>
+    <h1>Holaaaaaaaaa</h1>
+
+
+
+</body>
+</html>
